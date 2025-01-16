f6b2f470abafc1956a784944873a15e131ddc5ad
MinUk <qjvk2880@gmail.com>
2025-01-16 16:12:26 +0900

    Feat : 테스트용 컨트롤러 추가

---
+++ src/main/java/com/demo/spring_rate_limiter/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.demo.spring_rate_limiter.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@GetMapping("/test")
+	public String test() {
+		return "Test Controller";
+	}
+}
