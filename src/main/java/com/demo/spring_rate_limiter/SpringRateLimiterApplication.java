e20702a81ad91e8fe2f8da69fdbc24be6abb26ce
MinUk <qjvk2880@gmail.com>
2025-01-16 16:09:09 +0900

    Initial Commit

---
+++ src/main/java/com/demo/spring_rate_limiter/SpringRateLimiterApplication.java
@@ -0,0 +1,13 @@
+package com.demo.spring_rate_limiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRateLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRateLimiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/spring_rate_limiter/SpringRateLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.spring_rate_limiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRateLimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
