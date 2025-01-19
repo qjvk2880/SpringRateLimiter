49d2858062534e29702ccf729fc4c3b84ffdb983
MinUk <qjvk2880@gmail.com>
2025-01-19 20:15:33 +0900

    Feat : @EnableScheduling 추가

--- src/main/java/com/demo/spring_rate_limiter/SpringRateLimiterApplication.java
+++ src/main/java/com/demo/spring_rate_limiter/SpringRateLimiterApplication.java
@@ -2,8 +2,10 @@ package com.demo.spring_rate_limiter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringRateLimiterApplication {
 
 	public static void main(String[] args) {
